31077d1f2c79dad19b958da9ba3de8bea6522d7b
Yulong <cs.yulong@gmail.com>
2019-01-24 11:11:12 -0500

    Add more utils to socket.js and pass user to server when connecting

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,20 +1,67 @@
 <template>
   <div class="chat">
     <h2>Devteam Chat</h2>
+
+    <div class="message-list-container">
+      <MessageList :messages="messages" />
+    </div>
+
+    <div class="message-form-container">
+      <MessageForm @submit="sendMessage" />
+    </div>
   </div>
 </template>
 
 <script>
 import Socket from "@/utils/socket";
+import Utils from "@/utils/utils";
+import MessageList from "./MessageList";
+import MessageForm from "./MessageForm";
 
 export default {
   name: "Chat",
+  components: {
+    MessageList,
+    MessageForm
+  },
   data() {
     return {
-      socket: new Socket()
-      // socket.sendMessage(username, avatar, message)
-      // socket.onReceiveMessage(callback) 
+      username: "",
+      avatar: "",
+      messages: [],
+      socket: null
     };
+  },
+  methods: {
+    sendMessage(data) {
+      this.socket.sendMessage(this.username, this.avatar, data.message);
+    }
+  },
+  mounted() {
+    const user = Utils.getUserInfo();
+    this.username = user.username;
+    this.avatar = user.avatar;
+
+    this.socket = new Socket(user);
+
+    this.socket.onReceiveMessage(data => {
+      this.messages = [...this.messages, data];
+      // you can also do this.messages.push(data)
+    });
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.chat {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  .message-list-container {
+    flex: 1;
+    height: 70%;
+    overflow: auto;
+  }
+}
+</style>
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,13 +1,19 @@
 import io from "socket.io-client";
 
 export default class Socket {
-  constructor() {
-    this.socket = io("http://uswanzi.com");
-    // this.socket = io("http://localhost:3001");
+  constructor(user) {
+    const options = {
+      reconnection: false,
+      query: {
+        ...user
+      }
+    };
+    this.socket = io("http://uswanzi.com", options);
+    // this.socket = io("http://localhost:3001", options);
   }
 
   sendMessage(username, avatar, message) {
-    this.socket.emit("SEND_MESSAGE", {
+    this.socket.emit("send_message", {
       username,
       avatar,
       message
@@ -15,8 +21,20 @@ export default class Socket {
   }
 
   onReceiveMessage(callback) {
-    this.socket.on("MESSAGE", data => {
+    this.socket.on("message", data => {
       callback(data);
+    });
+  }
+
+  onUserConnected(callback) {
+    this.socket.on("user_connected", data => {
+      callback(data);
+    });
+  }
+
+  onConnectionError(callback) {
+    this.socket.on("connect_error", error => {
+      callback(error);
     });
   }
 }
